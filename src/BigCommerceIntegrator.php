0bc7579d11a7a7c108962f4342fae9102cc5d95f
Agbitor Cosman <=>
2017-10-26 16:02:23 +0000

    Made createInstance method static

--- src/BigCommerceIntegrator.php
+++ src/BigCommerceIntegrator.php
@@ -81,7 +81,7 @@ class BigCommerceIntegrator
      * @param RepositoryWriterInterface $repositoryWriter
      * @return \Maverickslab\Integration\BigCommerce\BigCommerceIntegrator
      */
-    public function createInstance(string $authToken, string $clientId, string $clientSecret, string $storeId, RepositoryWriterInterface $repositoryWriter = null): self
+    public static function createInstance(string $authToken, string $clientId, string $clientSecret, string $storeId, RepositoryWriterInterface $repositoryWriter = null): self
     {
         return new static($authToken, $clientId, $clientSecret, $storeId, $repositoryWriter);
     }
